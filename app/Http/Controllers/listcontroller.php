5198b946e73f44cabe0283f267a6615cc233c210
ChielDiekio <chielv.dijk@hotmail.nl>
2022-05-30 20:54:40 +0200

    Commentaar toegevoed

--- app/Http/Controllers/listcontroller.php
+++ app/Http/Controllers/listcontroller.php
@@ -2,49 +2,43 @@
 
 namespace App\Http\Controllers;
 
+//import models
 use App\Models\Games;
 use App\Models\Gamedevs;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class listcontroller extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
 
-
-    //functie die data uit 'Gamedavs; haalt en stuurt naar de index.blade view1
     public function index()
-        {
-            $gamedevs = Gamedevs::all();
-            return view("index", [ 'gamedevs' => $gamedevs]);
-        }
+    {
+        //selecteerd alle data uit de gamedevs tabel
+        $gamedevs = Gamedevs::all();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Gamedevs  $gamedevs
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
+        //geeft de data aan de index.blade view in variable $gamedevs
+        return view("index", ['gamedevs' => $gamedevs]);
+    }
 
-//    public function show(Gamedevs $gamedevs)
-//    {
-//    echo "test";
-//        return view('games.show')->with('Gamedevs',$gamedevs);
-//    }
 
-// Functie die die data laat zien aan hand van de id van gamedevs.naam
-    public function showdata($gamedev){
+    public function showdata($gamedev)
+    {
+        //maakt connection met games tabel
         $games = DB::table('games')
-            ->select('games.id','games.titel', 'games.uitgave datum', 'games.platform')
+
+            //pakt de data: id, titel, uitgave datum, en platform uit de games tabel
+            ->select('games.id', 'games.titel', 'games.uitgave datum', 'games.platform')
+
+            //gamedevs_id is gelijk aan gamedev_id in de 'games' tabel
             ->join('gamedevs', 'games.gamedev_id', '=', 'gamedevs.id')
+
+            //selecteert de data van tabel waar naam gelijk is aan gamedevs_naam
             ->where('gamedevs.naam', '=', $gamedev)
             ->get();
 
-        return view("games.show", [ 'games' => $games]);
+        //geeft de data aan de show.blade view in variable $games
+        return view("games.show", ['games' => $games]);
     }
 
 }
--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -4,6 +4,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{--load bootstrap--}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 </head>
@@ -14,7 +16,7 @@
     <thead class="thead-dark">
     <tr>
         <th scope="col">#</th>
-        <th scope="col" >Titel Game:</th>
+        <th scope="col">Titel Game:</th>
         <th scope="col">Uitgave datum:</th>
         <th scope="col">Te spelen op:</th>
     </tr>
@@ -23,8 +25,10 @@
 
     @foreach($games as $game)
 
+        {{--fix voor spatie error--}}
         <?php $game->uitgave_datum = $game->{'uitgave datum'}; ?>
         <tr>
+
             <td>{{ $game->id }} </td>
             <td>{{ $game->titel }} </td>
             <td>{{ $game->uitgave_datum}} </td>
